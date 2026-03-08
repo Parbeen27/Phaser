0bc25a51efa30f67121b22d80ff0fa95ecfbd3ff
Parbeen27 <parbeensingh27@gmail.com>
2026-03-08 17:18:16 +0530

    mobile controls update

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,8 +4,11 @@ export default class MenuScene extends Phaser.Scene {
     }
     create() {
         this.add.text(100, 100, "Welcome to the Game!", { font: "32px Arial", fill: "#ffffff" });
-        this.add.text(100, 200, "Press SPACE to Start", { font: "24px Arial", fill: "#ffffff" });
+        this.add.text(100, 200, "Press SPACE or TAP to Start", { font: "24px Arial", fill: "#ffffff" });
         this.input.keyboard.on('keydown-SPACE', () => {
+            this.scene.start('gameScene');
+        });
+        this.input.on('pointerdown', () => {
             this.scene.start('gameScene');
         });
     }
